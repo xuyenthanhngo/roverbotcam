94336bc2f8b17605bf9664a895f83f99192e5cb2
xuyennt <xuyennt@vng.com.vn>
2017-02-04 18:27:48 +0700

    - update

--- robotracking.cpp
+++ robotracking.cpp
@@ -483,7 +483,7 @@ static void video_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffe
 
 					if (match != -1) {
 						
-						putText(greyImg, symbols[match].name, Point(320, 30), 1,
+						putText(origImage, symbols[match].name, Point(320, 30), 1,
 								2, Scalar(0, 255, 0), 2);
 						//char sTmp[256];
 						//sprintf (sTmp, "Symbol %s\n", symbols[match].name.c_str());
