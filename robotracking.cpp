6bb0f659fbe2c35b05232ef95ad0d0293fd40221
xuyennt <xuyennt@vng.com.vn>
2017-02-04 17:24:48 +0700

    - update

--- robotracking.cpp
+++ robotracking.cpp
@@ -373,9 +373,139 @@ static void video_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffe
 		}
 		cvtColor(origImage, greyImg, CV_RGB2GRAY);
 		
+		//Detect symbols
+		Mat canny_output;
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+
+		GaussianBlur(greyImg, greyImg, Size(9, 9), 2, 2);
+
+		/// Detect edges using canny
+		Canny(greyImg, canny_output, lowThreshold, lowThreshold * 3, 3);
+
+		//	imshow("B",canny_output);
+		/// Find contours
+		findContours(canny_output, contours, hierarchy, CV_RETR_TREE,
+				CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+		vector<Point> approxRect;
+
+		for (size_t i = 0; i < contours.size(); i++) {
+			approxPolyDP(contours[i], approxRect,
+					arcLength(Mat(contours[i]), true) * 0.05, true);
+			if (approxRect.size() == 4) {
+				float area = contourArea(contours[i]);
+
+				if (area > 10000) {
+					std::vector<cv::Point2f> corners;
+
+					vector<Point>::iterator vertex;
+					vertex = approxRect.begin();
+					//vertex++;
+					circle(camera, *vertex, 2, Scalar(0, 0, 255), -1, 8, 0);
+					corners.push_back(*vertex);
+					vertex++;
+					circle(camera, *vertex, 2, Scalar(0, 0, 255), -1, 8, 0);
+					corners.push_back(*vertex);
+					vertex++;
+					circle(camera, *vertex, 2, Scalar(0, 0, 255), -1, 8, 0);
+					corners.push_back(*vertex);
+					vertex++;
+					circle(camera, *vertex, 2, Scalar(0, 0, 255), -1, 8, 0);
+					corners.push_back(*vertex);
+
+					Moments mu;
+					mu = moments(contours[i], false);
+					Point2f center(mu.m10 / mu.m00, mu.m01 / mu.m00);
+
+					sortCorners(corners, center);
+
+					// Define the destination image
+					Mat correctedImg = ::Mat::zeros(195, 271, CV_8UC3);
+
+					// Corners of the destination image
+					std::vector<cv::Point2f> quad_pts;
+					quad_pts.push_back(Point2f(0, 0));
+					quad_pts.push_back(Point2f(correctedImg.cols, 0));
+					quad_pts.push_back(
+							Point2f(correctedImg.cols, correctedImg.rows));
+					quad_pts.push_back(Point2f(0, correctedImg.rows));
+
+					// Get transformation matrix
+					Mat transmtx = getPerspectiveTransform(corners, quad_pts);
+
+					// Apply perspective transformation
+					warpPerspective(camera, correctedImg, transmtx,
+							correctedImg.size());
+
+					Mat correctedImgBin;
+
+					cvtColor(correctedImg, correctedImgBin, CV_RGB2GRAY);
+
+					//equalizeHist(correctedImgBin, correctedImgBin);
+
+					correctedImgBin.copyTo(new_image);
+
+					threshold(correctedImgBin, correctedImgBin, 140, 255, 0);
+
+					imshow("B", correctedImgBin);
+
+					double minVal,maxVal,medVal;
+
+
+					minMaxLoc(new_image, &minVal, &maxVal);
+
+					medVal=(maxVal-minVal)/2;
+
+					threshold(new_image, new_image, medVal, 255, 0);
+
+					imshow("C", new_image);
+
+
+					Mat diffImg;
+
+					int match, minDiff, diff;
+					minDiff = 12000;
+					match = -1;
+
+					for (int i = 0; i < 8; i++) {
+						//diffImg = symbols[i].img-correctedImgBin;
+
+						bitwise_xor(new_image, symbols[i].img, diffImg,
+								noArray());
+
+						diff = countNonZero(diffImg);
+						if (diff < minDiff) {
+							minDiff = diff;
+							match = i;
+						}
+
+						if (i == 0) {
+							//	imshow("B",diffImg);
+
+						}
+					}
+
+					//imshow("B", correctedImg);
+
+					if (match != -1) {
+						putText(camera, symbols[match].name, Point(320, 30), 1,
+								2, Scalar(0, 255, 0), 2);
+					}
+					//break;
+
+				}
+
+			}
+		}		
+		//End detect symbols
+		
+		//Detect ball
 		int serial = 0;
 		threshedImage = Mat::zeros( origImage.size(), CV_8U );
 	    threshImage( origImage, threshedImage, serial );
+		//End detect ball
+		
 		// Show the result:
 		imshow("orig", origImage);
 		imshow( "thresh", threshedImage );
@@ -662,6 +792,8 @@ int main(int argc, const char **argv)
 		trace("Successful reading reference symbols\n");
 	}
 	
+	createTrackbar("Min Threshold:", "A", &lowThreshold, 100, CannyThreshold);
+	
     // Our main data storage vessel..
     RASPIVID_STATE state;
 
@@ -686,6 +818,8 @@ int main(int argc, const char **argv)
     cvNamedWindow("orig", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("thresh", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("gray", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("B", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("C", CV_WINDOW_AUTOSIZE);
 	trace("CV_WINDOW_AUTOSIZE : ok");
     int w=state.width;
     int h=state.height;
